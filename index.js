88d7a894999535ee7e9f4b8b4eb08e9070906dee
Aparna Ashok <ashok.a@husky.neu.edu>
2020-12-02 00:34:26 -0800

    Params modified

--- index.js
+++ index.js
@@ -3,49 +3,20 @@ aws.config.update({ region: "us-east-1" });
 const ses = new aws.SES();
 exports.handler = function(event, context, callback) {
     const message = event.Records[0].Sns.Message;
-    let data = JSON.parse(message);
+    let publishedData = JSON.parse(message);
     console.log("Message -------------" +message);
-    console.log("Data -------------" +data);
-    let emailParams = {
-        Destination: {
-            ToAddresses: [
-                data.Email
-            ]
-        },
-        Message: {
-            Body: {
-                Text: {
-                    Charset: "UTF-8",
-                    Data: data.Message
-                }
-            },
-            Subject: {
-                Charset: "UTF-8",
-                Data:data.Subject
-            }
-        },
-        Source: "webapp@"+process.env.DOMAINNAME
-    };
-
+    console.log("Data -------------" +publishedData);
     let ddb = new aws.DynamoDB({apiVersion: '2012-08-10'});
-    let id  = "" + data.Email + data.Time;
-    let storeData = {
-        TableName: "csye6225",
-        Item: {
-            id: {S: id},
-            data: { S: message}
-        }
-    };
+    let id  = "" + publishedData.Email + publishedData.Time;
     let getData = {
         TableName: 'csye6225',
         Key: {
             'id': { S: id}
         },
     };
-
-    ddb.getItem(getData, function(err, data) {
-        if(err) {
-            console.log(err);
+    ddb.getItem(getData, function(error, data) {
+        if(error) {
+            console.log(error);
         }
         else {
             console.log("Get DATA---" + data);
@@ -55,18 +26,45 @@ exports.handler = function(event, context, callback) {
             let dataItem = JSON.stringify(data.Item);
             console.log("Get dataItem---" +dataItem);
             if (data.Item === null || data.Item === undefined) {
-                ddb.putItem(storeData, function(err, data) {
-                    if(err) {
-                        console.log(err);
+                let storeData = {
+                    TableName: "csye6225",
+                    Item: {
+                        id: {S: id},
+                        data: { S: message}
+                    }
+                };
+                ddb.putItem(storeData, function(error, data) {
+                    if(error) {
+                        console.log(error);
                     } else{
+                        let emailParams = {
+                            Destination: {
+                                ToAddresses: [
+                                    publishedData.Email
+                                ]
+                            },
+                            Message: {
+                                Body: {
+                                    Text: {
+                                        Charset: "UTF-8",
+                                        Data: publishedData.Message
+                                    }
+                                },
+                                Subject: {
+                                    Charset: "UTF-8",
+                                    Data:publishedData.Subject
+                                }
+                            },
+                            Source: "webapp@"+process.env.DOMAINNAME
+                        };
                         const sendPromise = ses.sendEmail(emailParams).promise();
                         sendPromise
                             .then(data => {
                                 console.log("Added to DDB----- " + data.Message);
                                 callback(null, "Success");
                             })
-                            .catch(err => {
-                                console.error(err, err.stack);
+                            .catch(error => {
+                                console.error(error, error.stack);
                                 callback(null, "Failed");
                             });
                     }
