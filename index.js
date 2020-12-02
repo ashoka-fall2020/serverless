7c868c4450cd5b4a88b8ef331b83eb558a48e117
Aparna Ashok <ashok.a@husky.neu.edu>
2020-12-02 00:26:30 -0800

    Code cleaning

--- index.js
+++ index.js
@@ -6,9 +6,30 @@ exports.handler = function(event, context, callback) {
     let data = JSON.parse(message);
     console.log("Message -------------" +message);
     console.log("Data -------------" +data);
+    let emailParams = {
+        Destination: {
+            ToAddresses: [
+                data.Email
+            ]
+        },
+        Message: {
+            Body: {
+                Text: {
+                    Charset: "UTF-8",
+                    Data: data.Message
+                }
+            },
+            Subject: {
+                Charset: "UTF-8",
+                Data:data.Subject
+            }
+        },
+        Source: "webapp@"+process.env.DOMAINNAME
+    };
+
     let ddb = new aws.DynamoDB({apiVersion: '2012-08-10'});
     let id  = "" + data.Email + data.Time;
-    let writeData = {
+    let storeData = {
         TableName: "csye6225",
         Item: {
             id: {S: id},
@@ -30,33 +51,14 @@ exports.handler = function(event, context, callback) {
             console.log("Get DATA---" + data);
             let jsonData = JSON.stringify(data);
             console.log("Get JSONDATA---" +jsonData);
+            console.log("Get JSONDATA---" +data.Item);
             let dataItem = JSON.stringify(data.Item);
             console.log("Get dataItem---" +dataItem);
             if (data.Item === null || data.Item === undefined) {
-                ddb.putItem(writeData, function(err, data) {
+                ddb.putItem(storeData, function(err, data) {
                     if(err) {
                         console.log(err);
                     } else{
-                        let emailParams = {
-                            Destination: {
-                                ToAddresses: [
-                                    data.Email
-                                ]
-                            },
-                            Message: {
-                                Body: {
-                                    Text: {
-                                        Charset: "UTF-8",
-                                        Data: data.Message
-                                    }
-                                },
-                                Subject: {
-                                    Charset: "UTF-8",
-                                    Data:data.Subject
-                                }
-                            },
-                            Source: "webapp@"+process.env.DOMAINNAME
-                        };
                         const sendPromise = ses.sendEmail(emailParams).promise();
                         sendPromise
                             .then(data => {
